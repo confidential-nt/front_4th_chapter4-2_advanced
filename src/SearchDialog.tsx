bafeb8a4c90a421adb08176f090ab45a95d772eb
confidential-nt <youyoy689@gmail.com>
2025-02-26 16:21:21 +0900

    perf: fetch에 캐시 적용

--- src/SearchDialog.tsx
+++ src/SearchDialog.tsx
@@ -32,7 +32,7 @@ import {
 import { useScheduleContext } from './ScheduleContext.tsx';
 import { Lecture } from './types.ts';
 import { parseSchedule } from './utils.ts';
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 import { DAY_LABELS } from './constants.ts';
 
 interface Props {
@@ -87,14 +87,33 @@ const fetchLiberalArts = () =>
   axios.get<Lecture[]>('/schedules-liberal-arts.json');
 
 // TODO: 이 코드를 개선해서 API 호출을 최소화 해보세요 + Promise.all이 현재 잘못 사용되고 있습니다. 같이 개선해주세요.
+const createCachedFetcher = () => {
+  const cache = new WeakMap();
+
+  return async (fetcher: () => Promise<AxiosResponse<Lecture[], unknown>>) => {
+    if (cache.has(fetcher)) {
+      return cache.get(fetcher);
+    }
+
+    const result = await fetcher();
+    cache.set(fetcher, result);
+    return result;
+  };
+};
+
+const fetchWithCache = createCachedFetcher();
+
 const fetchAllLectures = async () =>
   await Promise.all([
-    (console.log('API Call 1', performance.now()), fetchMajors()),
-    (console.log('API Call 2', performance.now()), fetchLiberalArts()),
-    (console.log('API Call 3', performance.now()), fetchMajors()),
-    (console.log('API Call 4', performance.now()), fetchLiberalArts()),
-    (console.log('API Call 5', performance.now()), fetchMajors()),
-    (console.log('API Call 6', performance.now()), fetchLiberalArts()),
+    (console.log('API Call 1', performance.now()), fetchWithCache(fetchMajors)),
+    (console.log('API Call 2', performance.now()),
+    fetchWithCache(fetchLiberalArts)),
+    (console.log('API Call 3', performance.now()), fetchWithCache(fetchMajors)),
+    (console.log('API Call 4', performance.now()),
+    fetchWithCache(fetchLiberalArts)),
+    (console.log('API Call 5', performance.now()), fetchWithCache(fetchMajors)),
+    (console.log('API Call 6', performance.now()),
+    fetchWithCache(fetchLiberalArts)),
   ]);
 
 // TODO: 이 컴포넌트에서 불필요한 연산이 발생하지 않도록 다양한 방식으로 시도해주세요.
